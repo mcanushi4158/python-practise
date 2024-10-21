e9a379cdc947e92c0a7a7a006fce9b569195cd9f
mcanushi4158 <mcanushi4158@sbc.edu>
2024-10-21 00:08:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 print ("hello World")
+print("my life is not going ok")
+x= "statics"
+y= "Linear Alegbra"
+z= "ddw"
+m="statistics"
+print(x)
+print(y)
+print(z)
+print(m)
+
+
+n = int(input())
+if n % 2 != 0:
+    print ("Weird")
+elif n % 2 == 0 and 2 < n< 5:
+    print ("Not Weird")
+elif n % 2 == 0 and 6 < n < 20:
+    print("Weird")
+elif n % 2 == 0 and n ==20:
+    print("Weird")
+elif n % 2 == 0 and n > 20 :
+    print ("Not Weird")
+
+
+    if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    x = int(a//b)
+    y = float(a/b)
+    print(x)
+    print(y)
